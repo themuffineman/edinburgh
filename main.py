7ea82741a263171bba797e12c8cd42913686cdac
themuffineman <petrusheya@gmail.com>
2025-09-02 11:10:57 +0200

    switch to html content for message

--- main.py
+++ main.py
@@ -19,27 +19,30 @@ recipients = [
 ]
 
 SUBJECT = "Hello Old Friend. Ready for the tech-con?"
-BODY = """
-Hi there Petrus,
 
-Just checking in if everything is ok? 
-If you need me to acompany you to the tech coference, just say the word
-
-"""
 
 def send_email(to_address):
-    msg = MIMEMultipart()
+    msg = MIMEMultipart("alternative")
     msg["From"] = EMAIL_ADDRESS
     msg["To"] = to_address
     msg["Subject"] = SUBJECT
+    html = ""
+    with open("mail.html", "r") as file:
+        html = file.read()
 
-    msg.attach(MIMEText(BODY, "plain"))
-
+    html_part = MIMEText(html, 'html')
+    msg.attach(html_part)
+    print("Begining sending process...")
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-            server.send_message(msg)
+            status_code, response = server.ehlo()
+            print(f"[*] Echoing the server: {status_code} {response}")
+            status_code, response= server.starttls()
+            print(f"[*] Starting TLS connection: {status_code} {response}")
+            status_code, response = server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            print(f"[*] Logging in: {status_code} {response}")
+            server.sendmail(EMAIL_ADDRESS, to_address, msg.as_string())
+            server.quit()
             print(f"[+] Email sent to {to_address}")
     except Exception as e:
         print(f"[!] Failed to send to {to_address}: {e}")
@@ -51,4 +54,4 @@ for recipient in recipients:
     print(f"Waiting {delay} seconds before next email...")
     time.sleep(delay)
 
-print("All emails sent!")
+print("Done")
