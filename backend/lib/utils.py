bba36b0cbf1a2de400d7b882a5abbe8f34d398b1
themuffineman <petrusheya@gmail.com>
2025-09-05 10:46:12 +0200

    reverted to single page and added plain text formatter

--- backend/lib/utils.py
+++ backend/lib/utils.py
@@ -6,6 +6,7 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 from pydantic import BaseModel
+from strip_markdown import strip_markdown
 import json
 
 
@@ -17,69 +18,37 @@ class IdealUrls(BaseModel):
 
 
 def extract_info_from_website(url:str) -> str:
-
+        max_tokens_per_page = 100000
         summarry_prompt = """
             Use the makrdown from the website below and write me a description of what the business is about. 
             Here's the content of a website. 
         """
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True, timeout=60000)
+            browser = p.chromium.launch(headless=False, timeout=60000, )
             page = browser.new_page()
             page.goto(url)
             page_url = page.url
-            if page_url.endswith("/"):
-                page_url = page_url[:-1]
-            print("page url is", page_url)
+            if(page_url.endswith("/")):
+                 page_url = page_url[:-1]
+                 print("Formated Url without / -->", page_url)
             body_html = page.query_selector("body").inner_html()
-            a_tags = page.query_selector_all("a")
-            hrefs = []
-            for tag in a_tags:
-                  hrefs.append(tag.get_attribute("href"))
-            hrefs = [href for href in hrefs if "#" not in href or href.startswith("/") == False]
-            hrefs = set(hrefs)
-            hrefs = list(hrefs)
+            markdowns = [convert_to_markdown(body_html)]
+            for markdown_text in markdowns:
+                plain_text = strip_markdown(markdown_text)
+                print(plain_text)
+                if((len(plain_text)/4) > max_tokens_per_page): 
+                    print("Too expensive to scrape")
+                    return
+                response = client.responses.create(
+                    model="gpt-4.1-nano",
+                    input=f"""
+                        {summarry_prompt}
 
-            ideal_urls_res = client.responses.parse(
-                model="gpt-4.1-nano",
-                input=[
-                        {"role": "system", "content": "Extract the event information."},
-                        {
-                            "role": "user",
-                            "content": f"""
-                                        We are building a docier of this business by scraping someof their web pages.
-                                        But of course manyof these are irrelavant and not valid to extract any meaningful data about the business
-                                        like pivacy-policies, sitemaps. Filter this list of urls to include only the most high value page that we can extract 
-                                        themost information from inorder to persoanlize our out reach 
-                                        -------------------------------
-                                        {hrefs}
-                                    """,
-                        },
-                ],
-                text_format=IdealUrls
-            )
-            ideal_urls:list[str] = json.loads(ideal_urls_res.output_text)
-            for ideal_url in ideal_urls:
-                page.goto(f"{page_url}{ideal_url}")
+                        ---------------------------------
 
-
-
-
+                        {plain_text}
+                    """
+                )
+                print("Summary is: ", response.output_text)
             browser.close()
-
-            return
-            markdown = convert_to_markdown(body_html)
-            plain_text = re.sub(r'[*_`#>-]', '', markdown)
-            response = client.responses.create(
-                model="gpt-4.1-nano",
-                input=f"""
-                    {summarry_prompt}
-
-                    ---------------------------------
-
-                    {plain_text}
-                """
-            )
-            print("Summary is: ", response.output_text)
-            browser.close()
-
 extract_info_from_website("https://looca.app")
