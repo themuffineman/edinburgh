3d1b2a62206ebcc0e7291c319300f3ccae491afd
themuffineman <petrusheya@gmail.com>
2025-09-08 09:20:35 +0200

    added logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,16 +5,20 @@ import { LeadsTable } from "@/components/Table";
 import { CSVUploader } from "@/components/CSVUploader";
 import { Lead } from "@/components/types";
 import { toast } from "sonner";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans items-center justify-items-center min-h-screen w-full">
-      <main className="flex flex-col h-max gap-18 p-20 py-24 justify-center items-center w-full max-w-full">
-        <h1 className="relative z-20 sm:text-6xl text-4xl ">
-          <span className="bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 font-bold text-transparent ">
-            Edinburgh
-          </span>{" "}
-          🕰️
+    <div className="font-sans items-center relative justify-items-center min-h-screen w-full">
+      <div className="[background-image:url('/noise.png')] -z-10 opacity-5 w-full h-full absolute top-0" />
+      <main className="flex flex-col h-max gap-18 p-20 py-5 justify-center items-center w-full max-w-full">
+        <h1 className="relative z-20 ">
+          <Image
+            src={"/edin-logo.png"}
+            width={300}
+            height={150}
+            alt="Edin logo"
+          />
         </h1>
         <div className="w-full max-w-full flex flex-col items-start gap-2 overflow-x-auto">
           <LeadsTable />
